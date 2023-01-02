3dc0c906fab914ff4d9238798224bc7e2b8c7ed3
Valerii Uskov <uskovvobdt@gmail.com>
2023-01-02 11:48:06 +0100

    test commit for pull request

--- src/main/java/com/example/teapot_project/model/User.java
+++ src/main/java/com/example/teapot_project/model/User.java
@@ -24,7 +24,7 @@ public class User {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
-                ", age)))=" + age +
+                ", age=" + age +
                 '}';
     }
 
