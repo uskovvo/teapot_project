203be0bdbc27effa56cf3224b4c89e2f58adf022
Valerii Uskov <uskovvobdt@gmail.com>
2023-01-02 17:58:11 +0100

    add new PropertiesUtil

---
+++ src/main/java/com/example/teapot_project/utils/PropertiesUtil.java
@@ -0,0 +1,29 @@
+package com.example.teapot_project.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public final class PropertiesUtil {
+
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        loadProperties();
+    }
+
+    public PropertiesUtil() {
+    }
+
+    private static void loadProperties() {
+        try (var inputStream = PropertiesUtil.class.getClassLoader().getResourceAsStream("application.properties")) {
+            PROPERTIES.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String get(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+}
