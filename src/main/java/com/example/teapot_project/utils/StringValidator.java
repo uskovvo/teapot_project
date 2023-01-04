92eabd9a30415f4af8e9ebdd477a176b26a1d65d
Fedor <fed-er-al@yandex.ru>
2023-01-04 15:31:08 +0400

    test changes

--- src/main/java/com/example/teapot_project/utils/StringValidator.java
+++ src/main/java/com/example/teapot_project/utils/StringValidator.java
@@ -15,7 +15,7 @@ public class StringValidator {
 
     private static boolean validation(String parameterToValidate, String nameOfParameter) {
         if (parameterToValidate == null || parameterToValidate.length() < 2 || parameterToValidate.length() > 30) {
-            throw new NotValidDataException(String.format("%s length must not be less than 2 and higher than 30"));
+            throw new NotValidDataException(("%s length must not be less than 2 and higher than 30"));
         }
         if (!parameterToValidate.matches("[a-zA-Zа-яА-Я -]+")) {
             throw new NotValidDataException(String.format("%s must contain only letters", nameOfParameter));
--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -1,26 +1,68 @@
 package dao;
 
 import com.example.teapot_project.dao.UserDao;
+import com.example.teapot_project.exceptions.DatabaseConnectionException;
+import com.example.teapot_project.exceptions.NotValidDataException;
 import com.example.teapot_project.model.User;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.example.teapot_project.utils.DataSource;
+import org.junit.jupiter.api.*;
 import utils.UserTestData;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 class UserDaoTest {
-    private static UserDao userDao = UserDao.getInstance();
+    private  UserDao userDao;
+   private Connection connection;
 
+
+    @BeforeEach
+    public void before() {
+        try {
+            Path path = Path.of("src/main/resources/populate-db.sql");
+            String table = Files.readString(path);
+            this.connection = DataSource.getConnection();
+            this.userDao = UserDao.getInstance();
+            PreparedStatement statement = connection.prepareStatement(table);
+            statement.execute();
+        } catch (SQLException | IOException e) {
+
+
+            throw new DatabaseConnectionException("Can't get database connection", new FileNotFoundException());
+        }
+    }
+    @AfterEach
+    public void after() throws SQLException {
+        this.connection.close();
+    }
 
 
     @Test
-    @DisplayName("Works if there is 2 users in db")
+    @DisplayName("Checks if there is 4 users in db")
     void getAll(){
         List<User> users = userDao.readAll();
-        Assertions.assertEquals(2, users.size());
+        Assertions.assertEquals(4, users.size());
+    }
+    @Test
+    @DisplayName("Checks if there is 1 users where Groupid=1")
+    void getAllWithGroupId(){
+        List<User> users = userDao.readAll(1);
+        Assertions.assertEquals(1, users.size());
     }
 
+
+    @Test
+    @DisplayName("Checks if there is 5 users where Groupid=1")
+    void getAllWithMockGroupId(){
+        List<User> users = userDao.readAll(1);
+        Assertions.assertEquals(1, users.size());
+    }
 
 
     @Test
@@ -49,4 +91,30 @@ class UserDaoTest {
         User newUser = userDao.create(UserTestData.NEW_USER);
         Assertions.assertTrue(userDao.delete(newUser.getId()));
     }
+
+    @DisplayName("Test methods throw NotValidDataException with wrong id ")
+    @Test
+
+    void NotValidDataExceptionTestId(){
+        Assertions.assertThrows(NotValidDataException.class, () -> UserTestData.NEW_USER.setId(-1L));
+        Assertions.assertThrows(NotValidDataException.class, () -> UserTestData.NEW_USER.setId(null));
+    }
+
+    @DisplayName("Test methods throw NotValidDataException with wrong Age ")
+    @Test
+    void NotValidDataExceptionTestAge(){
+        Assertions.assertThrows(NotValidDataException.class, () -> UserTestData.NEW_USER.setAge(-11));
+        Assertions.assertThrows(NotValidDataException.class, () -> UserTestData.NEW_USER.setAge(444));
+    }
+
+    @DisplayName("Test methods throw NotValidDataException with wrong Name ")
+    @Test
+    void NotValidDataExceptionTestName(){
+        Assertions.assertThrows(NotValidDataException.class, () -> UserTestData.NEW_USER.setName(""));
+        Assertions.assertThrows(NotValidDataException.class, () -> UserTestData.NEW_USER.setName("444"));
+        Assertions.assertThrows(NotValidDataException.class, () -> UserTestData.NEW_USER.setName("ddddd4"));
+        Assertions.assertThrows(NotValidDataException.class, () -> UserTestData.NEW_USER.setName("o"));
+    }
+
+
 }
