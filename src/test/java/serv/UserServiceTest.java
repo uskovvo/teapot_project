63f05d2eaec3310a745d7501075c53dab64702ec
valerii <uskoovvospb@gmail.com>
2023-01-05 17:49:36 +0100

    test jenkins 2.20 with test

---
+++ src/test/java/serv/UserServiceTest.java
@@ -0,0 +1,12 @@
+package serv;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void startCompetition() {
+    }
+}
